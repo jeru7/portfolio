5d2594b9db8c25d2d06e5cf4ec0d400d70e390b2
Emmanuel Ungab <john.ungab7@gmail.com>
2025-12-19 03:18:10 +0800

    add: tagline section

---
+++ components/tagline/Tagline.tsx
@@ -0,0 +1,19 @@
+const Tagline = () => {
+  return (
+    <section className="h-screen">
+      <article>
+        <p>
+          <span className="text-accent">byte by byte.</span>
+          <br />
+          That's how I learn.
+          <br />
+          <span className="text-accent">plan. design. code.</span>
+          <br />
+          That's how I build.
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default Tagline;
