d7edec5f8696b6a2eb912814d645d6735a131e5e
Emmanuel Ungab <john.ungab7@gmail.com>
2025-12-30 17:09:42 +0800

    edit: navbar click navigates to hero section

--- components/_ui/Navbar.tsx
+++ components/_ui/Navbar.tsx
@@ -1,11 +1,33 @@
 "use client";
 
 import { PERSONAL_INFO } from "@/constants/personal";
+import { useSwiperContext } from "@/context/swiper/SwiperContext";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const { swiper } = useSwiperContext();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleBack = () => {
+    if (pathname === "/" && swiper) {
+      swiper.slideTo(0, 700);
+      return;
+    }
+
+    router.push("/#home");
+  };
+
   return (
-    <nav className="w-full p-2 fixed top-0 bg-background z-10">
-      <h1 className="text-accent font-bold text-base">{PERSONAL_INFO.NAME}</h1>
+    <nav className="w-full fixed top-0 bg-background z-10 flex items-center justify-center">
+      <div className="p-2 w-full min-w-80 md:w-200">
+        <button
+          className="text-accent font-bold text-base hover:cursor-pointer"
+          onClick={handleBack}
+        >
+          {PERSONAL_INFO.NAME}
+        </button>
+      </div>
     </nav>
   );
 };
